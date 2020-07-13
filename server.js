bf788f05bd730fea9f638fda11bf2700d2f2ff88
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-12 23:28:30 -0500

    Created get request to populate notes on the page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Adds our dependencies.
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 // Assigns the server to port 8080 and stores the invocation of express within a variable.
 var app = express();
@@ -10,13 +11,30 @@ var PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Connects the stylesheet so it can be referenced by our html files.
+app.use(express.static(__dirname + '/public'))
+
+// Returns the notes.html file as a response
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
+// Returns the index.html file as a response when a request is sent via the root route
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
+
+app.get('/api/notes', function(req, res) {
+    fs.readFile('db/db.json', 'utf-8', function(err, data) { 
+        console.log(data)
+        return res.json(JSON.parse(data));
+    })
+})
+
+app.post('/api/notes', function(req, res) {
+
+})
+
 
 
 
