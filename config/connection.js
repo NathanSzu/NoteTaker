3f9bf0cbcad05bbd321c75a4daa9f1fa3a40aa6b
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-21 09:47:59 -0500

    Fixed mysql heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
 const util = require('util');
+const connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = msql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Sokka1992!',
     database: 'noteTaker_db'
   });
+};
+
 
 connection.connect(function(err) {
   if (err) {
