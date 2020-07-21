3cd54c40ea320a7960195033c6dc1acd71c872a7
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-20 22:02:51 -0500

    Finished ORM

--- config/orm.js
+++ config/orm.js
@@ -5,9 +5,20 @@ class ORM {
         this.connection = connection
     }
 
-    
+    selectAll(){
+        const queryString = 'SELECT * FROM notes'
+        return this.connection.query(queryString)
+    }
 
+    createNote(values){
+        const queryString = `INSERT INTO notes (title, text) VALUES (?, ?)`
+        return this.connection.query(queryString, [...values])
+    }
     
+    deleteNote(value){
+        const queryString = `DELETE FROM notes WHERE id = ?`
+        return this.connection.query(queryString, [value])
+    }
 
 }
 
